c9bd7a4eedab28757702560861d5c76ea3247a74
vinayakgoyal29 <vgoyal7_be25@thapar.edu>
2025-09-20 14:49:26 +0530

    Q9.c

--- lab 2/assignment 2/Q9.c
+++ lab 2/assignment 2/Q9.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    printf("Size of char: %zu byte(s)\n", sizeof(char));
-    printf("Size of int: %zu byte(s)\n", sizeof(int));
-    printf("Size of long int: %zu byte(s)\n", sizeof(long int));
-    printf("Size of float: %zu byte(s)\n", sizeof(float));
-    printf("Size of double: %zu byte(s)\n", sizeof(double));
-    return 0;
-}
