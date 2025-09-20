f2739bd68a2a79022a555b99cf3e24c53d902d4e
vinayakgoyal29 <vgoyal7_be25@thapar.edu>
2025-09-20 14:48:08 +0530

    Q8.c

--- lab 3/assignment 3/Q8.c
+++ lab 3/assignment 3/Q8.c
@@ -1,1 +1,34 @@
+//Write a program to read in 5 numbers and compute the average, max, min, standard deviation.
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c,d,e,max,min,avg,stdev;
+      printf("\nEnter any Five Numbers:");
+      scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
+      max=a;
+      if(max<b)
+      max=b;
+      if(max<c)
+      max=c;
+      if(max<d)
+      max=d;
+      if(max<e)
+      max=e;
+      printf("MAX:%f",max);
+      min=a;
+      if(min>b)
+      min=b;
+      if(min>c)
+      min=c;
+      if(min>d)
+      min=d;
+      if(min>e)
+      min=e;
+      printf("\nMIN:%f",min);
+      avg=(a+b+c+d+e)/5;
+      printf("\nAVERAGE:%f",avg);
+      stdev=sqrt(pow((a-avg),2))+pow((b-avg),2)+pow((c-avg),2)+pow((d-avg),2)+pow((e-avg),2)/2;
+      printf("\nSTANDARD DEVIATION:%f\n",stdev);
+}
 
